96a80777dcf4de610d4c0bd2087f2ba9d7777dd4
Kurt Claussen <sevencsr@gmail.com>
2020-06-03 23:52:15 -0400

    Changes and Additions:     * Revamped part of playhand(), removing menu idea from "wager"       system and adding new wager system. Also, added code to process       wager from call to wager function.     * Created wager() function to get bet amount and process entry.     * Removed pretty much all of the code in main(). Now it only calls       gobj() to play the game.     * Removed call to titleprint() in gobj() as it works better       elsewhere.
    To-Do-Next:
        * Continue to make changes to playhand() and add the code for
          playblackjack() -- which is pretty much the core of the game.
          I'm sure that playblackjack will require several functions and a
          lot of thought. May take a few days. :)

--- GoJack.go
+++ GoJack.go
@@ -15,19 +15,20 @@ import (
 )
 
 func main() {
-	// Clear Screen - From K-Util Package
-	kutil.ClearScreen()
 
-	// Print Game Title
-	titleprint()
-
-	// Run Game - Making this modular in case I decide to brek this out into
-	// another project like GoCasino.
+	// Run Game - Making this modular in case I decide to break this out into
+	// another project like GoCasino. Super easy to call from a main menu that way.
 	gobj()
+
 }
 
 func titleprint() {
-	// Function that exists exclusively to print the title.
+
+	// Clear the screen.
+
+	kutil.ClearScreen()
+
+	// Print out the game title. We'll probably use this a lot.
 
 	fmt.Println("GoJack -- Blackjack for the Terminal")
 	fmt.Println("KEC - 2020")
@@ -39,11 +40,6 @@ func gobj() { // Yay! It's time to play Blackjack!
 	// Print out intro and initialize money to "100" credits.
 
 	var credits int = 100
-
-	fmt.Println("Welcome to GoJack -- Blackjack for the terminal!")
-	fmt.Println("You start with", credits, "credits.")
-	fmt.Println("Let's see what you do with them. :)")
-	fmt.Println()
 
 	// Initialize Card Arrays
 	/* As of right now, only one deck. May add another dimension to store
@@ -128,11 +124,71 @@ func shuffledeck() [52][2]int { // Shuffle a deck of cards.
 // playhand - Plays a single hand of GoBJ then returns game status, credits,
 //            and current deck of playing cards.
 func playhand(cred int, dck [52][2]int) (bool, int, [52][2]int) {
-	var stillplaying bool
+	var stillplaying bool // Is the player still playing?
+	var validchoice bool  // Is the choice returned valid?
+	var bet int           // This is the amount of the bet.
 
-	stillplaying = false
-	cred = 69
-	dck[42][1] = 69
+	validchoice = false
+	for validchoice == false { // Loop until a valid choice is made.
+		titleprint()      // Clear the screen and print the title.
+		bet = wager(cred) // Wager prompt. Returns "0" for bet if quitting.
 
+		if bet >= 0 && bet <= cred { // OK, we're going to play BJ now!
+			validchoice = true
+			fmt.Println("Amount wagered:", bet)
+			//	cred, dck = playblackjack(cred, dck)
+		}
+
+		/* Only a bet that is "0" to quit or an amount that's within the
+		   credit range of the player is valid. Anything else, send them
+		   back to try again! */
+
+	}
+
+	cred = cred - bet // %%% For testing purposes.
+
+	switch { // OK, let's see what we do after the game is played.
+	case bet == 0: // We bet "0" on the wager screen.
+		// Chuck us back to the calling routine and
+		// say we aren't playing any more. Since it hits
+		// this check first, it'll quit regadless of how
+		// many credits we have.
+		stillplaying = false
+	case cred > 0: // We still have credits left and didn't bet zero.
+		// Keep playing.
+		stillplaying = true
+	case cred <= 0: // We're out of credits! No more game for us!
+		stillplaying = false
+		fmt.Println("You've run out of credits! Thanks for playing!")
+		kutil.Pause(5)
+	}
+
+	fmt.Println(stillplaying)
 	return stillplaying, cred, dck
+	// Return the flag that lets us know if we are still playing,
+	// our leftover creds, and what's left of the deck of cards.
+}
+
+func wager(creds int) int { // This routine grabs our bet! Or quits.
+	var betamt int // Bet amount.
+
+	fmt.Println("How much would you like to wager?")
+	fmt.Println("Credits:", creds)
+	fmt.Printf("Amount (0 to exit) > ")
+	fmt.Scan(&betamt) // Get our bet amount from the terminal.
+
+	switch { // OK.. let's see what we bet.
+	case betamt == 0: // Bet is 0. Quit the game.
+		fmt.Println("Thanks for playing!")
+	case betamt < 0: // Bet is negative. Invalid.
+		fmt.Println("Sorry, you can't wager negative amounts of credits.")
+	case betamt > creds: // Bet is more than we have. Also invalid.
+		fmt.Println("Sorry, you can't wager more credits than you have!")
+	case betamt > 0 && betamt <= creds: // Bet is valid amount. Continue.
+		fmt.Println("Ok, you're betting", betamt, "credits!")
+	}
+
+	kutil.Pause(2) // Pause for a couple of seconds to allow user to read the outcome.
+
+	return betamt // Return the amount that was bet.
 }
